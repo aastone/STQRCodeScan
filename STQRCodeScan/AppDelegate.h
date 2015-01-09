aeb3af83db7cb0df10486c25c8d547e419f5d6d0
aastone <stoneiswang@gmail.com>
2015-01-09 21:16:56 +0800

    Initial Commit

---
+++ STQRCodeScan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  STQRCodeScan
+//
+//  Created by stone on 1/9/15.
+//  Copyright (c) 2015 stone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
